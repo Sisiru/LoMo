50ad3515ecd3ab0800e2348bf72c29e27d2fb5a0
Ruveni Jayawardana <rsjaya29062@gmail.com>
2014-09-17 21:08:36 +0530

    Changed the codes of broadcastreceivers

--- LoMo/src/com/app/lomo/MyScheduleReceiver.java
+++ LoMo/src/com/app/lomo/MyScheduleReceiver.java
@@ -9,34 +9,33 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 
+//Broadcast receiver
 public class MyScheduleReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Bundle bundle = intent.getExtras();
+		//when receiving the broadcast signal this method will be called
+		Bundle bundle = intent.getExtras();//receiving the data from the TrackLocation activity
 		String bundleTime=bundle.getString("Time");
 		int time=0;
 		if(bundleTime==null){
-			time=1;
+			time=1;//this will be assigned when the device reboots
 		}else{
-			time = Integer.parseInt(bundleTime);
+			time = Integer.parseInt(bundleTime);//this is assigned all the other time
 		}
 		
+		//next intent that will be started
 		Intent i = new Intent(context, MyStartServiceReceiver.class);
-
+		//pending intent to wait for a predefined period
 		PendingIntent pending = PendingIntent.getBroadcast(context, 0, i, 0);
 		
 		Calendar cal = Calendar.getInstance();
-		// start 30 seconds after boot completed
-
+		//here we use the pending intent to wait a particular time before starting the background service again
 		cal.add(Calendar.MINUTE, time);
 
 		AlarmManager alarmManager = (AlarmManager) context
-				.getSystemService(Context.ALARM_SERVICE);
-		// fetch every REPEAT_TIME period
+				.getSystemService(Context.ALARM_SERVICE);//getting the alarm service
 		alarmManager.set(AlarmManager.RTC, cal.getTimeInMillis(), pending);
-		
-		
 	}
 
 }
--- LoMo/src/com/app/lomo/MyStartServiceReceiver.java
+++ LoMo/src/com/app/lomo/MyStartServiceReceiver.java
@@ -5,16 +5,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
-
+//broadcast receiver 
 public class MyStartServiceReceiver extends BroadcastReceiver {
 
 	@Override
 	public void onReceive(Context context, Intent intent) {
+		//this method is called when the MyScheduleReceiver wants to communicate
+		//MyScheduleReceiver waits for a particular period and broadcasts that 'Waiting time is over'
+		//This is heard by the MyStartServuceReceiver and calls the service class to start the service again
 		Log.i("Entry", "Broadcast receiver to call service");
-		Toast.makeText(context, "Start Again", Toast.LENGTH_SHORT).show();
-		Intent service = new Intent(context, TrackLocation.class);
-	    context.startService(service);
-	    
+		Toast.makeText(context, "Starts the service", Toast.LENGTH_SHORT).show();
+		Intent service = new Intent(context, TrackLocation.class);//calls the tracklocation service class
+	    context.startService(service);//starts the service    
 	}
-
 }
